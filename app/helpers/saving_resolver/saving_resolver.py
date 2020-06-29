3a8ceb8763c5dfd133578b09c76b0304694e4931
Nimantha Nadeeraka <info.nadeeraka@gmail.com>
2020-06-29 22:37:35 +0530

    improve

--- app/helpers/saving_resolver/saving_resolver.py
+++ app/helpers/saving_resolver/saving_resolver.py
@@ -1,5 +1,20 @@
+import functools
 from typing import List
 from app.models import Saving
 
-def saving_resolver(savings:List[Saving]):
-    print(savings)
+
+def saving_resolver(savings: List[Saving]):
+    pass
+
+
+def saving_re(arg):
+    saving_list = list(arg)
+    if len(saving_list) == 0:
+        return  -1
+    sum_of_saving = functools.reduce(lambda a, b: a + b, saving_list)
+    if sum_of_saving < 50000:
+        return (sum_of_saving/100)*3
+    elif sum_of_saving < 499999.99:
+        return (sum_of_saving/100)*4
+    elif sum_of_saving > 500000:
+        return (sum_of_saving/100)*5
