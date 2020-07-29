c3b1397a15ce6efda08b2f2ecc8a09a4d8cf1c36
Nimantha Nadeeraka <info.nadeeraka@gmail.com>
2020-07-29 17:22:11 +0530

    savings modified

---
+++ app/core/savings.py
@@ -0,0 +1,13 @@
+class Savings:
+    # 24 -> 8.25 %
+    # 36 -> 8.50%
+    # 48 -> 9.00
+
+    def __init__(self):
+        pass
+
+    def fixed_savings_for_full_year(self, rate,months ):
+        pass
+
+    def normal_savings(self):
+        pass
--- app/helpers/saving_resolver/saving_resolver.py
+++ app/helpers/saving_resolver/saving_resolver.py
@@ -1,44 +1,22 @@
 import functools
 # from typing import List
 # from app.models import Saving
+from app.helpers.util import calculate
 
 
 class Saving:
-    def __init__(self,rate,amount,time):
-        self.rate = rate
-        self.amount = amount
-        self.time = time  # time in month
 
+    def calculate_saving_algo(self, amount):
+        rate = 0
+        if amount < 50000:
+            rate = 3
+        elif 499999.99 < amount > 50000:
+            rate = 4
 
-    def set_vars(self,r,a,t):
-        self.rate = r
-        self.amount = a
-        self.time = t
+        elif amount > 499999.99:
+            rate = 5
+        return rate * amount
 
-    def calculate_savings(self):
-        sum =  float(self.amount)/float(self.rate*self.time)
-        return  sum/12
-
-s = Saving(4,32000,1)
-# s.set_vars(4,32000,1)
-print(s.calculate_savings())
-
-
-
-
-# def saving_resolver(savings: List[Saving]):
-#     pass
-#
-# # for 0ne year
-#
-# def saving_reate(arg,t):
-#     saving_list = list(arg)
-#     if len(saving_list) == 0:
-#         return  -1
-#     sum_of_saving = functools.reduce(lambda a, b: a + b, saving_list)
-#     if sum_of_saving < 50000:
-#         return (sum_of_saving/100)*3
-#     elif sum_of_saving < 499999.99:
-#         return (sum_of_saving/100)*4
-#     elif sum_of_saving > 500000:
-#         return (sum_of_saving/100)*5
+    def calculate_savings(self, amount_array, months):
+        amount = calculate(amount_array)
+        return self.calculate_saving_algo(amount) / months
